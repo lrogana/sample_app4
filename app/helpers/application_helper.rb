e0ae5028821aaa0401fdb2263027251e96e2ebb2
lrogana <2ndcenter@gmail.com>
2016-11-22 08:00:45 -0800

    Add a full_title helper

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,12 @@
 module ApplicationHelper
+  #Returns te full title on a per page basis
+  def full_title(page_title = '')
+    base_title = "Tournament App"
+    if page_title.empty?
+      base_title
+    else
+      page_title + " | " + base_title
+    end
+  end
+
 end
